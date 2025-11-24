c91124a2a6776e898fc84420be787c9f45246564
pluuto960 <189347604+pluuto960@users.noreply.github.com>
2025-11-24 12:12:43 +0200

    tektsifunktsioonid töö
    Added hints for guessing the name of an Estonian city.

--- content/tekstfunktsioonid.php
+++ content/tekstfunktsioonid.php
@@ -8,7 +8,7 @@ function clearVarsExcept($url, $varname){
 }
 
 echo'<br>';
-$linn="sss";
+$linn="Põlva";
 echo "<ol><li>Linn algab ".substr($linn,0,1)." tähega</li>";
 ?>
 <form action="<?=clearVarsExcept($_SERVER['REQUEST_URI'], "leht")?>" method="post">
@@ -16,12 +16,27 @@ echo "<ol><li>Linn algab ".substr($linn,0,1)." tähega</li>";
     <input type="text" id="linn" name="linn">
     <input type="submit" value="Kontrolli">
 </form>
+
 <?php
-//if(isset($_REQUEST["linn"])){
-//    if($_REQUEST["linn"]=="sss"){
-//
-//    }
-//}
+if(isset($_REQUEST["linn"])){
+    if($_REQUEST["linn"]=="Põlva"){
+        echo $_REQUEST["linn"]." - Linnanimi Õige";
+    } else{
+        echo $_REQUEST["linn"]." - Linnanimi vale";
+    }
+}
 
 
-
+echo "<h2>MÕISTATUS – ARVA ÄRA EESTI LINNANIMI</h2>";
+/* eesmärk on ära arvata, millist Eesti linna on kirjeldatud.
+// Kirjuta abiks 5–6 tekstipõhist funktsiooni” ehk vihjet,
+// mis aitavad samm-sammult lähemale jõuda õigele linnanimele.
+// funktsioonid on näiteks selliseid - esimene täht on .. jne*/
+//Näiteks,
+echo '<br>';
+$linn="Põlva";
+echo "<ol><li>Linn algab ".substr($linn, 0,1)." tähega</li>";
+echo "<li>Linna nimi on ".strlen($linn)." tähte</li>";
+echo "<li>Linn on Volkswagen Passatite pealinn</li>";
+echo"<li>Linn lõppeb ".substr($linn, -1)." tähega</li>";
+echo"<li>Linn asub Lõuna-Eestis</li>";
